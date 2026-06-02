3948ddd70d7c4f11c8bdb8cb008f2d6f98471d8f
Mohammed Amaan <mohammed.amaan@sombank.so>
2026-06-02 23:51:28 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayout,
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./features/dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
+      },
+    ],
+  },
+];
--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,29 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sidebar } from '../sidebar/sidebar/sidebar';
-import { SidebarService } from '../../shared/services/sidebar.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [CommonModule, RouterOutlet, Sidebar],
+  imports: [CommonModule, RouterOutlet],
   template: `
-    <div class="d-flex">
-      <app-sidebar></app-sidebar>
-      <div class="flex-grow-1 with-sidebar" [class.sidebar-collapsed]="isSidebarCollapsed">
+
         <router-outlet></router-outlet>
-      </div>
-    </div>
+
   `,
   styleUrl: './main-layout.scss',
 })
 export class MainLayout implements OnInit {
-  isSidebarCollapsed = false;
-  private readonly sidebarService = inject(SidebarService);
 
   ngOnInit(): void {
-    this.sidebarService.collapsed$.subscribe((collapsed) => {
-      this.isSidebarCollapsed = collapsed;
-    });
+
   }
 }
