8b103fbbb11adaeb6ddd6d276671d18598eb8500
Mohammed Amaan <mohammed.amaan@sombank.so>
2026-06-03 11:25:45 +0530

    update footer

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Footer } from '../../../shared/components/footer/footer';
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, Footer],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
--- src/app/features/auth/signup/signup.ts
+++ src/app/features/auth/signup/signup.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { Footer } from '../../../shared/components/footer/footer';
 
 @Component({
   standalone: true,
   selector: 'app-sign-up',
-  imports: [RouterModule],
+  imports: [RouterModule, Footer],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
