2da09aaa532b54c56e940c10c74d8efa4a5fcf40
Mohammed Amaan <mohammed.amaan@sombank.so>
2026-06-03 11:14:27 +0530

    update signup page

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -3,23 +3,37 @@ import { Login } from './login/login';
 import { VerifyOtp } from './verify-otp/verify-otp';
 import { ForgetPassword } from './forget-password/forget-password';
 import { ResetPassword } from './reset-password/reset-password';
+import { Signup } from './signup/signup';
 
 export const AUTH_ROUTES: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login, title: 'Business Portal | Login' },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login,
+    title: 'HealSync | Login'
+  },
+  {
+    path: 'signup',
+    component: Signup,
+    title: 'HealSync | Sign Up'
+  },
   {
     path: 'verify-otp',
     component: VerifyOtp,
-    title: 'Business Portal | Verify OTP',
+    title: 'HealSync | Verify OTP',
   },
   {
     path: 'forget-password',
     component: ForgetPassword,
-    title: 'Business Portal | Forget Password',
+    title: 'HealSync | Forget Password',
   },
   {
     path: 'reset-password',
     component: ResetPassword,
-    title: 'Business Portal | Reset Password',
+    title: 'HealSync | Reset Password',
   },
 ];
